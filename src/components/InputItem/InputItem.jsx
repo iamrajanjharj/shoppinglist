2c31ca88f2eab2c8cba6fea72c40c7da57591d45
Rajan Jha <jharajan20@gmail.com>
2024-11-12 23:13:00 -0600

    Added input tag and button to add items to shopping list

---
+++ src/components/InputItem/InputItem.jsx
@@ -0,0 +1,13 @@
+//CSS Imports
+import "./InputItem.css";
+
+function InputItem() {
+  return (
+    <div className="item-input-wrapper">
+      <input className="item-input" type="text" placeholder="Add an item..." />
+      <button className="add-item-button">Add</button>
+    </div>
+  );
+}
+
+export default InputItem;
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,9 +1,17 @@
+//Component Imports
 import Header from "../Header/Header";
+import InputItem from "../InputItem/InputItem";
+
+//CSS Imports
+import "./ShoppingList.css";
 
 function ShoppingList() {
   return (
     <>
       <Header />
+      <div className="current-shopping-list">
+        <InputItem />
+      </div>
     </>
   );
 }
